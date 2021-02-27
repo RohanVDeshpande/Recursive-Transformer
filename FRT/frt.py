b7dddbd8101233c624ee28a64e1775c590ab3386
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2021-02-26 21:59:44 -0500

    Testing with simple 1 data point example

--- FRT/frt.py
+++ FRT/frt.py
@@ -11,12 +11,6 @@ END = u"\u2352"					# र
 PADDING = u"\u2340"				# त
 LEN = 16
 
-# x = torch.Tensor([[[1, 2]], [[3, 4]], [[5, 6]],[[7, 8]], [[9, 10]],[[11, 12]]])
-# print(x, x.shape)
-# #print(x.view(1, -1), x.view(1, -1).shape)
-# print(x.view(-1, 2), x.view(-1, 2).shape)
-
-
 srctext = "5+5" + (LEN-3) * PADDING
 tgttext = "10" + TGT_LOOP_SEP + "1" + END + (LEN - 5) * PADDING
 
@@ -34,6 +28,14 @@ FEATURES = 16
 
 #print(src_mask)
 #print(tgt_mask)
+def text2tensor(dictionary, text):
+	return torch.tensor([ dictionary.word2idx[c] for c in text], dtype=torch.long)
+
+def tensor2text(dictionary, t):
+	if len(t.shape) == 1:
+		return "".join([dictionary.idx2word[idx] for idx in t])
+	else:
+		return ""
 
 class FRT(nn.Module):
 	""" Forced Recursive Transformer """
@@ -44,6 +46,7 @@ class FRT(nn.Module):
 		#src_key_padding_mask=src_mask, tgt_key_padding_mask=tgt_mask
 		self.embed = nn.Embedding(config["TOKENS"], config["FEATURES"])
 		self.transformer = nn.Transformer(d_model=config["FEATURES"], nhead=config["HEADS"], num_encoder_layers=config["ENC_LAYERS"], num_decoder_layers=config["DEC_LAYERS"], dim_feedforward=config["FEED_FORWARD"])
+		self.log_softmax = nn.LogSoftmax()
 		self.apply(self._init_weights)
 		print("number of parameters: {}".format(sum(p.numel() for p in self.parameters())))
 
@@ -52,18 +55,22 @@ class FRT(nn.Module):
 			module.weight.data.normal_(mean=0.0, std=0.02)
 
 	def forward(self, srctext, tgttext):
-		src_indicies = torch.tensor([ d.word2idx[c] for c in  srctext], dtype=torch.long)
+		src_indicies = text2tensor(d, srctext)
 		src = torch.unsqueeze(self.embed(src_indicies), 1)
-		#src_mask = torch.tensor([ float('-inf') if c == PADDING else 0 for c in  srctext]).view(1, -1)
+		src_padding_mask = torch.tensor([ float('-inf') if c == PADDING else 0 for c in  srctext]).view(1, -1)
 
-		tgt_indicies = torch.tensor([ d.word2idx[c] for c in  tgttext], dtype=torch.long)
+		tgt_indicies = text2tensor(d, tgttext)
 		tgt = torch.unsqueeze(self.embed(tgt_indicies), 1)
-		#tgt_mask = torch.tensor([ float('-inf') if c == PADDING else 0 for c in  tgttext]).view(1, -1)
+		tgt_padding_mask = torch.tensor([ float('-inf') if c == PADDING else 0 for c in  tgttext]).view(1, -1)
 		#print(src.shape, tgt.shape)
-		output = self.transformer(src, tgt, tgt_mask=self.transformer.generate_square_subsequent_mask(LEN))
+		output = self.transformer(src, tgt,
+								  tgt_mask=self.transformer.generate_square_subsequent_mask(LEN),
+								  src_key_padding_mask=src_padding_mask,
+								  tgt_key_padding_mask=tgt_padding_mask).view(-1, FEATURES)
+		output = self.log_softmax(output)
 		#print(output)
 
-		return output.view(-1, FEATURES), tgt_indicies
+		return output, tgt_indicies
 
 
 
@@ -81,7 +88,7 @@ optimizer = optim.Adam(model.parameters(), lr=0.0001)
 criterion = nn.NLLLoss()
 
 model.train()
-for epoch in range(100):
+for epoch in range(300):
     optimizer.zero_grad()
     output, tgt_ref = model(srctext, tgttext)
     #print(output.shape)
@@ -90,3 +97,14 @@ for epoch in range(100):
     loss.backward()
     optimizer.step()
     print("Loss: {}".format(loss.item()))
+
+model.eval()
+output, tgt_ref = model(srctext, tgttext)
+#print(output)
+#print(output.shape)
+got=torch.argmax(output, 1)
+print(got)
+print(tgt_ref)
+
+print("Expected: '{}'".format(tensor2text(d, tgt_ref)))
+print("Got: '{}'".format(tensor2text(d, got)))
