7ca82007528996069f7043367b57b942bc98eef7
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2021-03-02 21:30:10 -0500

    Tutorial files moved to its own folder

--- FRT/main.py
+++ FRT/main.py
@@ -1,235 +1,181 @@
-# coding: utf-8
-import argparse
-import time
 import math
+import random
+from termcolor import colored
+import json
+import utils
+from utils import AverageMeter
 import os
 import sys
+import time
+import datetime
+
 import torch
 import torch.nn as nn
-import torch.onnx
+import torch.nn.functional as F
+import torch.optim as optim
+import argparse
+import frt
 
 import data
-import model
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
 
-parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 RNN/LSTM/GRU/Transformer Language Model')
-parser.add_argument('--data', type=str, default='./data/wikitext-2',
-                    help='location of the data corpus')
-parser.add_argument('--emsize', type=int, default=200,
-                    help='size of word embeddings')
-parser.add_argument('--nhid', type=int, default=200,
-                    help='number of hidden units per layer')
-parser.add_argument('--nlayers', type=int, default=2,
-                    help='number of layers')
-parser.add_argument('--lr', type=float, default=20,
-                    help='initial learning rate')
-parser.add_argument('--clip', type=float, default=0.25,
-                    help='gradient clipping')
-parser.add_argument('--epochs', type=int, default=40,
-                    help='upper epoch limit')
-parser.add_argument('--batch_size', type=int, default=20, metavar='N',
-                    help='batch size')
-parser.add_argument('--bptt', type=int, default=35,
-                    help='sequence length')
-parser.add_argument('--dropout', type=float, default=0.2,
-                    help='dropout applied to layers (0 = no dropout)')
-parser.add_argument('--tied', action='store_true',
-                    help='tie the word embedding and softmax weights')
-parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed')
+from torch.utils.data import DataLoader
+
+
+parser = argparse.ArgumentParser(description='Forced Recursive Transformer')
+parser.add_argument('--mode', type=str, default=None, required=True,
+                    help="Choose train mode vs. test mode",
+					choices=["train", "test"])
+parser.add_argument('--data', type=str, required=True,
+                    help='Dataset path')
+parser.add_argument('--model-config', type=str, required=True,
+                    help='Model json config')
 parser.add_argument('--cuda', action='store_true',
                     help='use CUDA')
-parser.add_argument('--log-interval', type=int, default=200, metavar='N',
-                    help='report interval')
-parser.add_argument('--save', type=str, default='model.pt',
-                    help='path to save the final model')
-parser.add_argument('--onnx-export', type=str, default='',
-                    help='path to export the final model in onnx format')
-
-parser.add_argument('--nhead', type=int, default=2,
-                    help='the number of heads in the encoder/decoder of the transformer model')
-parser.add_argument('--dry-run', action='store_true',
-                    help='verify the code and the model')
+parser.add_argument('--params', type=str, help='Model params path for test')
 
 args = parser.parse_args()
 
+assert os.path.isdir("output/"), "You need a folder called 'output' in order to save model data / test predictions"
+assert os.path.isdir("tb/"), "You need a folder called 'tb' for tensorboard"
+
 # Set the random seed manually for reproducibility.
-torch.manual_seed(args.seed)
+# torch.manual_seed(args.seed)
 if torch.cuda.is_available():
     if not args.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    	utils.confirm("You have a CUDA device, so you should probably run with --cuda.")
+    else:
+    	print("Using CUDA device")
 
 device = torch.device("cuda" if args.cuda else "cpu")
 
-if os.path.exists(args.save):
-    print("The following file will be overwritten when the model saves: {}".format(args.save))
-    while True:
-        reply = str(input("Are you sure you want to proceed? [y/n]")).lower().strip()
-        if reply == "y":
-            print("Proceeding...")
-            break
-        elif reply == "n":
-            print("Will not proceed...")
-            sys.exit(1)
 
-###############################################################################
-# Load data
-###############################################################################
+with open(args.model_config) as f:
+  model_config = json.load(f)
+  timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  model_path = "output/{}_{}.pt".format(model_config["NAME"], timestamp)
+  prediction_path = "output/{}_{}_{}.txt".format(model_config["NAME"], os.path.splitext(os.path.basename(args.data))[0], timestamp)
+  tb_log_path = "tb/{}_{}".format(model_config["NAME"], timestamp)
 
-corpus = data.Corpus(args.data)
+  if args.mode == "train" and os.path.exists(model_path):
+    utils.confirm("Re-training the model will overwrite the following file: {}".format(model_path))
+  elif args.mode == "test" and os.path.exists(prediction_path):
+   utils.confirm("Re-testing will cause the prediction output file to be overwritten: {}".format(prediction_path))
 
-# Starting from sequential data, batchify arranges the dataset into columns.
-# For instance, with the alphabet as the sequence and batch size 4, we'd get
-# ┌ a g m s ┐
-# │ b h n t │
-# │ c i o u │
-# │ d j p v │
-# │ e k q w │
-# └ f l r x ┘.
-# These columns are treated as independent by the model, which means that the
-# dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
-# batch processing.
+  with open(model_config["DATASET_CONFIG"]) as f:
+  	dataset_config = json.load(f)
 
-def batchify(data, bsz):
-    # Work out how cleanly we can divide the dataset into bsz parts.
-    nbatch = data.size(0) // bsz
-    # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    data = data.narrow(0, 0, nbatch * bsz)
-    # Evenly divide the data across the bsz batches.
-    data = data.view(bsz, -1).t().contiguous()
-    return data.to(device)
+dataset = data.Dataset(dataset_config)
+dataset.buildDataset(args.data)
+dataset.device = device
 
-eval_batch_size = 10
-train_data = batchify(corpus.train, args.batch_size)
-val_data = batchify(corpus.valid, eval_batch_size)
-test_data = batchify(corpus.test, eval_batch_size)
+dataloader = DataLoader(dataset, batch_size=dataset_config["BATCH_SIZE"], shuffle=dataset_config["SHUFFLE"], num_workers=dataset_config["WORKERS"],
+           pin_memory=dataset_config["PIN_MEMORY"], prefetch_factor=dataset_config["PREFETCH_FACTOR"],
+           persistent_workers=True, collate_fn=data.dataset_collate_fn)
 
-###############################################################################
-# Build the model
-###############################################################################
+model_config["TOKENS"] = dataset.tokens()
+model_config["SRC_LEN"] = dataset.SRC_LEN
+model_config["TGT_LEN"] = dataset.TGT_LEN
 
-ntokens = len(corpus.dictionary)
-model = model.TransformerModel(ntokens, args.emsize, args.nhead, args.nhid, args.nlayers, args.dropout).to(device)
+model = frt.FRT(model_config)
+model.device = device
+model.to(device)
 
-criterion = nn.NLLLoss()
+if args.mode == "train":
+	assert model_path is not None, "Model path not set up"
 
-###############################################################################
-# Training code
-###############################################################################
+	writer = SummaryWriter(tb_log_path, comment=utils.config2comment(model_config, dataset_config))
 
-# get_batch subdivides the source data into chunks of length args.bptt.
-# If source is equal to the example output of the batchify function, with
-# a bptt-limit of 2, we'd get the following two Variables for i = 0:
-# ┌ a g m s ┐ ┌ b h n t ┐
-# └ b h n t ┘ └ c i o u ┘
-# Note that despite the name of the function, the subdivison of data is not
-# done along the batch dimension (i.e. dimension 1), since that was handled
-# by the batchify function. The chunks are along dimension 0, corresponding
-# to the seq_len dimension in the LSTM.
+	if model_config["OPTIMIZER"] == "ADAM":
+		optimizer = optim.Adam(model.parameters(), lr=model_config["LR"])
+	elif model_config["OPTIMIZER"] == "ADAMW":
+		optimizer = optim.AdamW(model.parameters(), lr=model_config["LR"])
+	criterion = nn.NLLLoss(ignore_index=dataset.dictionary.word2idx[dataset.PADDING])
 
-def get_batch(source, i):
-    seq_len = min(args.bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
-    return data, target
+	EPOCHS = model_config["EPOCHS"]
 
+	model.train()
+	iteration=0
+	try:
+		for epoch in range(EPOCHS):
+			epoch_loss = 0.
+			with tqdm(total=len(dataset)) as prog:
+				batch_time = AverageMeter()
+				data_time = AverageMeter()
+				update_time = AverageMeter()
 
-def evaluate(data_source):
-    # Turn on evaluation mode which disables dropout.
-    model.eval()
-    total_loss = 0.
-    ntokens = len(corpus.dictionary)
-    with torch.no_grad():
-        for i in range(0, data_source.size(0) - 1, args.bptt):
-            data, targets = get_batch(data_source, i)
-            output = model(data)
-            output = output.view(-1, ntokens)
-            total_loss += len(data) * criterion(output, targets).item()
-    return total_loss / (len(data_source) - 1)
+				batch_start_time = time.time()
+				for i, (src_indicies, src_padding_mask, tgt_indicies, tgt_padding_mask) in enumerate(dataloader):
 
+					src_indicies = src_indicies.to(device)
+					src_padding_mask = src_padding_mask.to(device)
+					tgt_indicies = tgt_indicies.to(device)
+					tgt_padding_mask = tgt_padding_mask.to(device)
+					#src_indicies = src_indicies.transpose(0, 1)
+					#tgt_indicies = tgt_indicies.transpose(0, 1)
 
-def train():
-    # Turn on training mode which enables dropout.
-    model.train()
-    total_loss = 0.
-    start_time = time.time()
-    ntokens = len(corpus.dictionary)
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
-        data, targets = get_batch(train_data, i)
-        # Starting each batch, we detach the hidden state from how it was previously produced.
-        # If we didn't, the model would try backpropagating all the way to start of the dataset.
-        model.zero_grad()
-        output = model(data)
-        output = output.view(-1, ntokens)
-        loss = criterion(output, targets)
-        loss.backward()
+					data_time.update(time.time() - batch_start_time) # data loading time
 
-        # `clip_grad_norm` helps prevent the exploding gradient problem.
-        torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
-        for p in model.parameters():
-            p.data.add_(p.grad, alpha=-lr)
+					update_start_time = time.time()
 
-        total_loss += loss.item()
+					optimizer.zero_grad()
+					output, tgt = model(src_indicies, tgt_indicies, src_padding_mask, tgt_padding_mask)
+					loss = criterion(output, tgt.view(-1))
+					loss.backward()
+					optimizer.step()
 
-        if batch % args.log_interval == 0 and batch > 0:
-            cur_loss = total_loss / args.log_interval
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
-                    'loss {:5.2f} | ppl {:8.2f}'.format(
-                epoch, batch, len(train_data) // args.bptt, lr,
-                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss)))
-            total_loss = 0
-            start_time = time.time()
-        if args.dry_run:
-            break
+					update_time.update(time.time() - update_start_time)
 
+					epoch_loss += loss.item()
+					prog.update(dataloader.batch_size)
+					
+					batch_time.update(time.time() - batch_start_time)
+					batch_start_time = time.time()
 
-def export_onnx(path, batch_size, seq_len):
-    print('The model is also exported in ONNX format at {}'.
-          format(os.path.realpath(args.onnx_export)))
-    model.eval()
-    dummy_input = torch.LongTensor(seq_len * batch_size).zero_().view(-1, batch_size).to(device)
-    hidden = model.init_hidden(batch_size)
-    torch.onnx.export(model, (dummy_input, hidden), path)
+					# if i % 16 == 0:
+					# 	print('avg data load time: {}\n'
+					# 		  'avg update time: {}\n'
+					# 		  'avg total batch time: {}'.format(data_time.avg, update_time.avg, batch_time.avg))
+					writer.add_scalar("Loss/train", loss.item(), iteration)
+					iteration += 1
+				epoch_loss /= len(dataset)
+				print("Epoch {}, Loss: {}".format(epoch, epoch_loss))
+	except:
+		print('-' * 89)
+		print('Exiting from training early')
+		
+	print('Saving model to {}'.format(model_path))
+	torch.save(model.state_dict(), model_path)
 
+elif args.mode == "test":
+	assert args.params is not None, "Model params path not set up"
+	model.load_state_dict(torch.load(args.params))
+	model.eval()
 
-# Loop over epochs.
-lr = args.lr
-best_val_loss = None
+	correct = 0
+	total = 0
 
-# At any point you can hit Ctrl + C to break out of training early.
-try:
-    for epoch in range(1, args.epochs+1):
-        epoch_start_time = time.time()
-        train()
-        val_loss = evaluate(val_data)
-        print('-' * 89)
-        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
-                'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
-                                           val_loss, math.exp(val_loss)))
-        print('-' * 89)
-        # Save the model if the validation loss is the best we've seen so far.
-        if not best_val_loss or val_loss < best_val_loss:
-            with open(args.save, 'wb') as f:
-                torch.save(model, f)
-            best_val_loss = val_loss
-        else:
-            # Anneal the learning rate if no improvement has been seen in the validation dataset.
-            lr /= 4.0
-except KeyboardInterrupt:
-    print('-' * 89)
-    print('Exiting from training early')
+	with open(prediction_path, "w") as f:
+		with tqdm(total=len(dataset)) as prog:
+			for (src_indicies, src_padding_mask, tgt_indicies, tgt_padding_mask) in dataloader:
+				src_indicies = src_indicies.to(device)
+				tgt_indicies = tgt_indicies.to(device)
+				src_padding_mask = src_padding_mask.to(device)
+				output = model.predict(src_indicies, src_padding_mask, dataset.dictionary.word2idx[dataset.START])
 
-# Load the best saved model.
-with open(args.save, 'rb') as f:
-    model = torch.load(f)
+				question_strings = [ q_str.split(dataset.PADDING)[0] for q_str in dataset.tensor2text(src_indicies)]
+				target_strings = [ tgt_str.split(dataset.END)[0] for tgt_str in dataset.tensor2text(tgt_indicies)]
+				output_strings = [ out_str.split(dataset.END)[0] for out_str in dataset.tensor2text(output)]
 
-# Run on test data.
-test_loss = evaluate(test_data)
-print('=' * 89)
-print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
-    test_loss, math.exp(test_loss)))
-print('=' * 89)
+				for j in range(len(target_strings)):
+					question = question_strings[j]
+					pred = output_strings[j]
+					actual = target_strings[j]
 
-if len(args.onnx_export) > 0:
-    # Export the model in ONNX format.
-    export_onnx(args.onnx_export, batch_size=1, seq_len=args.bptt)
+					print("Q: {} , A: {}".format(question, actual), file=f)
+					print("Got: '{}' {}\n".format(pred, "correct" if actual == pred else "wrong"), file=f)
+					correct += (actual == pred)
+					total += 1
+				prog.update(dataloader.batch_size)
+		print("{} Correct out of {} total. {:.3f}% accuracy".format(correct, total, correct/total * 100), file=f)
