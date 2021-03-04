e48005ca483e29eedbe8bea6b5b0401f3bf3b583
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2021-03-04 02:23:29 -0500

    PCA in 3d

--- FRT/embed_pca.py
+++ FRT/embed_pca.py
@@ -15,6 +15,7 @@ import data
 
 from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 parser = argparse.ArgumentParser(description='Forced Recursive Transformer')
@@ -23,6 +24,9 @@ parser.add_argument('--model-config', type=str, required=True,
 parser.add_argument('--params', type=str, required=True, help='Model path')
 parser.add_argument('--cuda', action='store_true',
                     help='use CUDA')
+
+parser.add_argument('--gif', action='store_true',
+                    help='Create a gif')
 
 
 args = parser.parse_args()
@@ -69,28 +73,41 @@ model.eval()
 
 X = model.embed.weight.detach().numpy()
 
-pca = PCA(n_components=2, whiten=True)
+pca = PCA(n_components=3, whiten=True)
 pca.fit(X)
 X_pca = pca.transform(X)
 print(X_pca.shape)
 
 print(X_pca)
 
-fig, ax = plt.subplots(1, 1)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
 delta = 0.015
 
 for i in range(len(dataset.dictionary.idx2word)):
-	ax.scatter(X_pca[i, 0], X_pca[i, 1])
+	ax.scatter(X_pca[i, 0], X_pca[i, 1], X_pca[i, 2])
 	if dataset.dictionary.idx2word[i] == dataset.PADDING:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "PADDING")
+		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "PADDING")
 	elif dataset.dictionary.idx2word[i] == dataset.START:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "START")
+		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "START")
 	elif dataset.dictionary.idx2word[i] == dataset.END:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "END")
+		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "END")
 	elif dataset.dictionary.idx2word[i] == dataset.TGT_LOOP_SEP:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "TGT_LOOP_SEP")
+		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "TGT_LOOP_SEP")
 	else:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, dataset.dictionary.idx2word[i])
+		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, dataset.dictionary.idx2word[i])
 fig.set_size_inches(8,6)
-fig.savefig("output/embedding_pca.png")
+
+
+def update(angle):
+    print("timestep {}".format(angle))
+    ax.view_init(30, angle)
+    return ax
+
+
+if args.gif is not None:
+	anim = FuncAnimation(fig, update, frames=range(0, 360), interval=150)
+	anim.save('output/embedding_pca.gif', dpi=80, writer='imagemagick')
+else:
+	fig.savefig("output/embedding_pca.png")
