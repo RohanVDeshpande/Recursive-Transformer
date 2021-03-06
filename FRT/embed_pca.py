fa210b21528d0fa7e339ccf23caeb7c91038fbed
Rohan Kalvade Deshpande <rohanvdeshpande@hotmail.com>
2021-03-06 16:34:30 -0500

    PCA embedding visualizer update

--- FRT/embed_pca.py
+++ FRT/embed_pca.py
@@ -6,6 +6,7 @@ import utils
 import os
 import sys
 
+import numpy as np
 import torch
 import torch.nn as nn
 import argparse
@@ -24,7 +25,11 @@ parser.add_argument('--model-config', type=str, required=True,
 parser.add_argument('--params', type=str, required=True, help='Model path')
 parser.add_argument('--cuda', action='store_true',
                     help='use CUDA')
-
+parser.add_argument('--dims', type=int, default=3,
+                    help="Number of dimensions",
+					choices=[2, 3])
+parser.add_argument('--num-only', action='store_true',
+                    help='Only consider numerical characters')
 parser.add_argument('--gif', action='store_true',
                     help='Create a gif')
 
@@ -48,7 +53,7 @@ device = torch.device("cuda" if args.cuda else "cpu")
 
 with open(args.model_config) as f:
   model_config = json.load(f)
-  dictionary_path = "output/dict/{}_dict.json".format(os.path.splitext(os.path.basename(args.params))[0])
+  dictionary_path = "output/dict/{}_dict.json".format(model_config["NAME"])
 
   with open(model_config["DATASET_CONFIG"]) as f:
   	dataset_config = json.load(f)
@@ -72,42 +77,85 @@ model.eval()
 
 
 X = model.embed.weight.detach().numpy()
+X = X[:len(dataset.dictionary.idx2word), :]
+print(X.shape)
 
-pca = PCA(n_components=3, whiten=True)
+if args.num_only:
+	print("Pruning dictionary & embedding space to only include numbers")
+	new_idx2word = []
+	for j in range(len(dataset.dictionary.idx2word) - 1, 0, -1):
+		if dataset.dictionary.idx2word[j].isdigit():
+			new_idx2word = [dataset.dictionary.idx2word[j]] + new_idx2word
+		else:
+			X = np.vstack((X[:j, :], X[j+1:,:]))
+	#print(X)
+	#print(X.shape)
+	print(new_idx2word)
+	dataset.dictionary.idx2word = new_idx2word
+pca = PCA(n_components=args.dims, whiten=True)
 pca.fit(X)
 X_pca = pca.transform(X)
-print(X_pca.shape)
+#print(X_pca.shape)
 
 print(X_pca)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+if args.dims == 2:
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
 
-delta = 0.015
+	delta = 0.015
 
-for i in range(len(dataset.dictionary.idx2word)):
-	ax.scatter(X_pca[i, 0], X_pca[i, 1], X_pca[i, 2])
-	if dataset.dictionary.idx2word[i] == dataset.PADDING:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "PADDING")
-	elif dataset.dictionary.idx2word[i] == dataset.START:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "START")
-	elif dataset.dictionary.idx2word[i] == dataset.END:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "END")
-	elif dataset.dictionary.idx2word[i] == dataset.TGT_LOOP_SEP:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "TGT_LOOP_SEP")
+	for i in range(len(dataset.dictionary.idx2word)):
+		ax.scatter(X_pca[i, 0], X_pca[i, 1])
+		if dataset.dictionary.idx2word[i] == dataset.PADDING:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "PADDING")
+		elif dataset.dictionary.idx2word[i] == dataset.START:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "START")
+		elif dataset.dictionary.idx2word[i] == dataset.END:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "END")
+		elif dataset.dictionary.idx2word[i] == dataset.TGT_LOOP_SEP:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "TGT_LOOP_SEP")
+		elif dataset.dictionary.idx2word[i] == dataset.LOOP_CONTINUE:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "LOOP_CONTINUE")
+		elif dataset.dictionary.idx2word[i] == dataset.LOOP_STOP:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, "LOOP_STOP")
+		else:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, dataset.dictionary.idx2word[i])
+	fig.set_size_inches(8,6)
+	fig.savefig("output/{}_embedding_pca_2d.png".format(model_config["NAME"]))
+elif args.dims == 3:
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+
+	delta = 0.015
+
+	for i in range(len(dataset.dictionary.idx2word)):
+		ax.scatter(X_pca[i, 0], X_pca[i, 1], X_pca[i, 2])
+		if dataset.dictionary.idx2word[i] == dataset.PADDING:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "PADDING")
+		elif dataset.dictionary.idx2word[i] == dataset.START:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "START")
+		elif dataset.dictionary.idx2word[i] == dataset.END:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "END")
+		elif dataset.dictionary.idx2word[i] == dataset.TGT_LOOP_SEP:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "TGT_LOOP_SEP")
+		elif dataset.dictionary.idx2word[i] == dataset.LOOP_CONTINUE:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "LOOP_CONTINUE")
+		elif dataset.dictionary.idx2word[i] == dataset.LOOP_STOP:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, "LOOP_STOP")
+		else:
+			ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, dataset.dictionary.idx2word[i])
+	fig.set_size_inches(8,6)
+
+
+	def update(angle):
+	    print("timestep {}".format(angle))
+	    ax.view_init(30, angle)
+	    return ax
+
+
+	if args.gif:
+		anim = FuncAnimation(fig, update, frames=range(0, 360), interval=150)
+		anim.save("output/{}_embedding_pca_3d.gif".format(model_config["NAME"]), dpi=80, writer='imagemagick')
 	else:
-		ax.text(X_pca[i, 0] + delta, X_pca[i, 1] + delta, X_pca[i, 2] + delta, dataset.dictionary.idx2word[i])
-fig.set_size_inches(8,6)
-
-
-def update(angle):
-    print("timestep {}".format(angle))
-    ax.view_init(30, angle)
-    return ax
-
-
-if args.gif is not None:
-	anim = FuncAnimation(fig, update, frames=range(0, 360), interval=150)
-	anim.save('output/embedding_pca.gif', dpi=80, writer='imagemagick')
-else:
-	fig.savefig("output/embedding_pca.png")
+		fig.savefig("output/{}_embedding_pca_3d.png".format(model_config["NAME"]))
